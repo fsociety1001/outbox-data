430b7643b4ce0acf9430e5c3fab190119acb2dba
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-10-10 11:06:41 -0300

    erlcron.hrl

---
+++ erlcron/include/erlcron.hrl
@@ -0,0 +1,12 @@
+%% compatibilidade
+-ifdef(OTP_RELEASE). %% 21 >
+-define(EXCEPTION(Class, Reason, Stacktrace), Class:Reason:Stacktrace).
+-define(GET_STACK(Stacktrace), Stacktrace).
+-include_lib("kernel/include/logger.hrl").
+-else.
+-define(EXCEPTION(Class, Reason, _), Class:Reason).
+-define(GET_STACK(_),        erlang:get_stacktrace()).
+-define(LOG_ERROR(Report),   error_logger:error_report(Report)).
+-define(LOG_WARNING(Report), error_logger:warning_report(Report)).
+-define(LOG_INFO(Report),    error_logger:info_report(Report)).
+-endif.
